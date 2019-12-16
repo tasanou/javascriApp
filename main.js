163146d18d836625bb80a67509414350eebbc34d
Zineb Ait Moussa <aitmoussazineb@gmail.com>
2019-12-17 00:45:14 +0100

    first push

---
+++ main.js
@@ -0,0 +1,7 @@
+function fetchIssues() {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById('issuesList');
+    issuesList.innerHTML = '';
+
+
+}
